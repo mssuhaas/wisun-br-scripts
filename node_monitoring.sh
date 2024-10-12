734c1437b86b66eb74162fe868bed4f8b3e65378
Surya Suhaas <mssuhaas@gmail.com>
2024-10-12 21:21:26 +0530

    updated Error Handling in node_monitoring.sh

--- node_monitoring.sh
+++ node_monitoring.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-
+cleanup() {
+    echo "Cleaning up and releasing resources..."
+    exit 0
+}
+trap cleanup SIGINT
 python3 /home/raspberrypi/Downloads/node_monitoring.py /home/raspberrypi/Downloads/recieved_logs.txt
